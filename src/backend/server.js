36551e267e7dba0a726ba52a5bef019f6b37f75a
Lukasz Walczak <lukeabove@gmail.com>
2019-12-27 11:30:04 +0100

    bcrypt added

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,27 +1,40 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
-
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
 
 app.use(express.json())
 
-app.get('/login', (req, res) => {
-  //  authenticate user 
-  // const username = req.body.username
-  // const user = { name: username }
-  const user = { name: 'maciek' }
-
-  // jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, null, (err, jwt) => {
-  //   console.log(jwt)
-  //   res.json({ jwt })
-  // })
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json(accessToken)
-
+app.get('/bcrypt', (req, res) => {
+  const saltRounds = 10;
+  const myPlaintextPassword = ';alkjf;aldifjaodi';
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function (err, hash) {
+      // Store hash in your password DB.
+      console.log('bcrypt hash:', hash)
+      res.send('you have been bcrypted, mate')
+    });
+  });
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+
+// app.get('/login', (req, res) => {
+//   //  authenticate user 
+//   // const username = req.body.username
+//   // const user = { name: username }
+//   const user = { name: 'maciek' }
+
+//   // jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, null, (err, jwt) => {
+//   //   console.log(jwt)
+//   //   res.json({ jwt })
+//   // })
+
+//   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//   res.json(accessToken)
+
+// })
+
+const port = 4000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
